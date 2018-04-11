de4387a381dac6407c41601de8ac90f0b6d314a4
Stephen Colfer <stephen.colfer@ucdconnect.ie>
2018-04-11 23:55:59 +0100

    HTML update

--- dublinbikes/app/views.py
+++ dublinbikes/app/views.py
@@ -1,9 +1,96 @@
-from flask import render_template
+from flask import render_template, jsonify, json, g, Flask
+from sqlalchemy import create_engine
 from app import app
+import os
+import json
+import sys
+import re
+import sqlite3
+from _sqlite3 import Row
 
-@app.route('/')
+#app = Flask(__name__, static_url_path='')
+#app.config.from_object('config')
+#app.config['SQL_ALCHEMY_URI'] = 'mysql://{dbuser}:{dbpassword1}@{dublinbikes.cww5dmspazsv.eu-west-1.rds.amazonaws.com}/{dublinbikes}'
+#db = SQLAlchemy(application)
+
+
+def connect_to_database():
+    engine = create_engine("mysql+mysqldb://dbuser:dbpassword1@dublinbikes.cww5dmspazsv.eu-west-1.rds.amazonaws.com/dublinbikes", echo=True)
+    return engine
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = connect_to_database()
+    return db
+
+#@app.teardown_appcontext
+#def close_connection(exception):
+    #db = getattr(g, '_database', None)
+    #if db is not None:
+        #db.close()
+#/
+@app.route("/stations")
+def get_all_stations():
+    engine = get_db()
+    #stations = []
+    sql = "SELECT number, name, latitude, longitude, bikes_available, stands_available from realtime;"
+    #rows = engine.execute("SELECT number, name, latitude, longitude from realtime;")
+    rows = engine.execute(sql).fetchall()
+    print("found {} stations", len(rows))
+    #for row in rows:
+        #stations.append(dict(row))
+    return jsonify(stations=[dict(row.items()) for row in rows])
+
+
+@app.route("/available/<int:station_id>")
+def get_stations(station_id):
+    engine = get_db()
+    data = []
+    
+    rows = engine.execute("SELECT bikes_available FROM realtime WHERE number = {};".format(station_id))
+
+    for row in rows:
+        data.append(dict(row))
+        
+    return jsonify(available=data)
+        
+@app.route('/station/<int:station_id>')
+def station(station_id):
+    return 'Retrieving station info for station number: {}'.format(station_id)
+
+
+@app.route('/', methods=['GET'])
 def index():
+    get_db()
+    get_all_stations()
+    #get_stations()
+    
+    #with app.open_resource('Dublin.json', 'r') as f:
+        #mydata = json.load(f)
+        #location = []
+        #name = []
+        #number = []
+        #address = []
+        #lat = []
+        #long = []
+        #j=0
+        #for i in mydata:
+        #    number.append(mydata[j]['number'])
+        #    name.append(mydata[j]['name'])
+        #    address.append(mydata[j]['address'])
+        #   lat.append(mydata[j]['latitude'])
+        #    long.append(mydata[j]['longitude'])
+
+        #    j+=1
+        #address = json.dumps(address).replace("\'", "\\'")
+        #name = json.dumps(name).replace("\'", "\\'")   
+        #number = json.dumps(number)
+        #lat = json.dumps(lat)
+        #long = json.dumps(long)    
     returnDict = {}
     returnDict['user'] = 'User123'
     returnDict['title'] = 'Dublin Bikes'
-    return render_template("index.html", **returnDict)
+    return render_template("index1.html", **returnDict)
+    # number=number, address=address, lat=lat, long=long
+
--- dublinbikes/dublinbikes.py
+++ dublinbikes/dublinbikes.py
@@ -10,27 +10,38 @@ from pprint import pprint
 from datetime import datetime
 
 
-
-
+#Import static data
 staticData = json.load(open('Dublin.json'))
-
+apiKey = "066552409dad0809af4e338d67817a8d931d697d"
 dubUrl = "https://api.jcdecaux.com/vls/v1/stations/30?contract=Dublin&apiKey=066552409dad0809af4e338d67817a8d931d697d"
 
         
 def query_API(stationNumber):
+    #Gets infromation for single station
     r = requests.get('https://api.jcdecaux.com/vls/v1/stations/' + str(stationNumber) + '?contract=Dublin&apiKey=' + apiKey)
     r = r.json() 
     return r
 
+def query_weather():
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Dublin&APPID=094f61b4b2da3c4541e43364bab71b0b')
+    r = r.json()
+    weatherInfo= {'main': r['weather'][0]['main'], 
+                     'detail': r['weather'][0]['description'], 
+                     'temp': r['main']['temp'], 
+                     'wind': r['wind']['speed']}
+    return r
+    
 def stations_list(fileName):
+    #Returns list of station numbers read from the static data
     data = json.load(open(fileName))
     stations = []
     for i in data:
         stations.append(i["number"])
         stations.sort()
     return stations
-
+    
 def timestamp_to_ISO(timestamp):
+    #Converts a timestamp to a readable time
     moment = datetime.fromtimestamp(timestamp / 1000)
     return moment.time().isoformat()
  
@@ -40,42 +51,55 @@ def info_csv():
     #Save information for all stations in a csv
     #-------------------------------------------
     with open('info.csv', 'w') as csvfile:
-        fieldnames = ['number', 'name', 'latitude', 'longitude', 'bikes', 'stands']
+        fieldnames = ['number', 'name', 'latitude', 'longitude', 'bikes', 'stands', 'time','date']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader() 
 
         for i in stations:
             g = query_API(i) 
+            d = datetime.now().date()
+            t = datetime.now().time()
             station_info = {'number': g["number"], 
                     'name': g["name"], 
                     'latitude': g["position"]["lat"], 
                     'longitude': g["position"]["lng"], 
                     'bikes': g["available_bikes"], 
-                    'stands': g["available_bike_stands"]}
+                    'stands': g["available_bike_stands"],
+                    'time': t,
+                    'date': d}
             writer.writerow(station_info)
     #-------------------------------------------
     
 def single_station_info(stationNumber):
+    #Creates a dictionary of the a single stations information
     g = query_API(stationNumber) 
+    d = datetime.now().date()
+    t = datetime.now().time()
     station_info = {'number': g["number"], 
-                    'name': g["name"], 
-                    'latitude': g["position"]["lat"], 
-                    'longitude': g["position"]["lng"], 
-                    'bikes': g["available_bikes"], 
-                    'stands': g["available_bike_stands"]}
-    
-    
+                     'name': g["name"], 
+                     'latitude': g["position"]["lat"], 
+                     'longitude': g["position"]["lng"], 
+                     'bikes': g["available_bikes"], 
+                     'stands': g["available_bike_stands"],
+                     'time': t,
+                     'date': d}
     return station_info
+        
         
     
 class Database:
     
-
     def __init__(self):
-        
+        #Connect to the database
         from mysql.connector import errorcode
         try:
-            
+            dhost="dublinbikes.cww5dmspazsv.eu-west-1.rds.amazonaws.com"
+            dport=3306
+            dbname="dublinbikes"
+            duser="dbuser"
+            dpassword="dbpassword1"
+            cnx = mysql.connector.connect(user = duser, password = dpassword, 
+                                      host = dhost, database=dbname)
             self.connection = cnx
             self.cur = cnx.cursor()
             print("connected")
@@ -85,26 +109,52 @@ class Database:
             elif err.errno == errorcode.ER_BAD_DB_ERROR:
                 print("Database does not exist")
             else:
-                print(err)
-#             else:
-#                 cnx.close()
-#             
+                print(err)  
          
         
     def add_station_info(self, statInfo):
-        query = "INSERT INTO stations (number, name, latitude, longitude, bikes_available, stands_available) " \
-                    "VALUES (%(number)s, %(name)s, %(latitude)s, %(longitude)s, %(bikes)s, %(stands)s) "
+        #Adds data for a single station to the database
+        query = "INSERT INTO stations (number, name, latitude, longitude, bikes_available, stands_available, time, date) " \
+                    "VALUES (%(number)s, %(name)s, %(latitude)s, %(longitude)s, %(bikes)s, %(stands)s, %(time)s, %(date)s) "
         
         self.cur.execute(query, statInfo)
         self.connection.commit()
-        print("info added")
         
+    
+    def realtime_info(self, statInfo):
+        #Inserts initial information of a single station into the real time table
+        query = "INSERT INTO realtime (number, name, latitude, longitude, bikes_available, stands_available, time) " \
+                    "VALUES (%(number)s, %(name)s, %(latitude)s, %(longitude)s, %(bikes)s, %(stands)s, %(time)s) "
         
+        self.cur.execute(query, statInfo)
+        self.connection.commit()
+        
+    def update_realtime(self, statInfo, i):
+        #Refreshes the information in the realtime table
+        query = "UPDATE realtime SET bikes_available = %(bikes)s, stands_available = %(stands)s, time = %(time)s WHERE number = " + str(i) 
+        self.cur.execute(query, statInfo)
+        self.connection.commit(),
+
+        
+    def print_db(self):
+        #Prints full realtime database
+        self.cur.execute("SELECT * FROM realtime ")
+        print(self.cur.fetchall())
+        
+    def print_station_info(self, j):
+        #Prints information for a single station
+        self.cur.execute("SELECT latitude, longitude, bikes_available, stands_available FROM realtime WHERE number = " + str(j))
+        print(self.cur.fetchall())
+        
+    def return_info(self, j):
+        self.cur.execute("SELECT bikes_available, stands_available FROM realtime WHERE number = " + str(j))
+        query = self.cur.fetchall()
+        info = {'bikes': query[0][0], 'stands': query[0][1]} 
+        return info
+    
     def close_db(self):
         self.cur.close()
         self.connection.close()
-    def test_thing(self):
-        return "thing"
       
 #print(stations_list('Dublin.json'))
 #print(query_API(55))
